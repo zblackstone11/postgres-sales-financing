127ddfac2750d755829c722d0ea8a7b64df66b09
Zack Blackstone <zblackstone11@yahoo.com>
2024-12-28 13:52:56 -0500

    Changed column order in customers table

--- schema/customers.sql
+++ schema/customers.sql
@@ -1,11 +1,11 @@
-CREATE TABLE Customers (
+CREATE TABLE Customers_new (
     customer_id SERIAL PRIMARY KEY,
-    account_number VARCHAR(10) NOT NULL UNIQUE,  -- Ensures unique account numbers
+    account_number VARCHAR(10) NOT NULL UNIQUE, -- Ensures unique account numbers
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     address TEXT NOT NULL,
-    phone_number VARCHAR(15) NULL,
     city VARCHAR(100),
     state VARCHAR(50),
-    zip_code VARCHAR(20)
+    zip_code VARCHAR(20),
+    phone_number VARCHAR(15) NULL
 );
